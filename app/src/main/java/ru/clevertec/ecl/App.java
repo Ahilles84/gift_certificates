6044348ce97872fa69d38580bfd7eace221014d6
Oleg Kuleshov <7100570@gmail.com>
2023-04-23 13:37:42 +0300

    build my first gradle project

---
+++ app/src/main/java/ru/clevertec/ecl/App.java
@@ -0,0 +1,11 @@
+package ru.clevertec.ecl;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
